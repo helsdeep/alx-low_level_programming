8ab85328f9ce3a8a706bf14bfe0ad4211e7ad900
helsdeep <femiossai@outlook.com>
2022-07-05 23:23:13 +0000

    the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,21 +2,17 @@
 /**
 *print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
 *followed by a new line
+*Return: 0
 */
 void print_alphabet_x10(void)
 {
-char ch;
-int i;
-i = 0;
-while (1 < 10)
+char c, i;
+for (i = 0; i <= 9; i++)
 {
-ch = 'a';
-while (ch <= 'z')
+for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(ch);
-ch++;
+_putchar(c);
 }
 _putchar('\n');
-i++;
 }
 }
