2ba637738bcb89bd193d1ab1d8e30363a0bfd97d
helsdeep <femiossai@outlook.com>
2022-07-09 22:42:45 +0000

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
-*main - prints the numbers from 1 to 100, followed by a new line
-*but for multiples of three prints Fizz instead of the number
-*and for the multiples of five prints Buzz
-*and for the multiples of both three and five print FizzBuzz
-*Retun: Always 0 (Success)
-*/
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
