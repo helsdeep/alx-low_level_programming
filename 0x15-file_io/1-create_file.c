a27137089ebb131c6f63a57eee32553fd1344981
helsdeep <femiossai@outlook.com>
2022-08-22 22:36:06 +0000

    file input and output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (f < 0)
+	if (fd < 0)
 		return (-1);
 
 	if (text_content)
